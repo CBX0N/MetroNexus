6f37b491215cb0c64ce29af6b9278da5d62cf283
Charlie Boon <charlie.boon@sky.com>
2025-08-22 22:51:29 +0100

    Adding LB

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,3 +5,21 @@ resource "cloudflare_dns_record" "server_record" {
   content = module.k3s_cluster.public_ipv4
   ttl     = 60
 }
+
+data "hcloud_load_balancers" "loadbalancers" {}
+
+locals {
+  loadbalancers = contains(data.hcloud_load_balancers.loadbalancers.load_balancers[*].name, "fsn-lb-01")
+}
+
+resource "cloudflare_dns_record" "cluster_record" {
+  count   = local.loadbalancers ? 1 : 0
+  name    = join(".", ["cluster", var.cloudflare_domain])
+  zone_id = var.cloudflare_dns_zone_id
+  type    = "A"
+  content = one([
+    for lb in data.hcloud_load_balancers.loadbalancers.load_balancers : lb.ipv4
+    if lb.name == "fsn-lb-01"
+  ])
+  ttl = 60
+}
